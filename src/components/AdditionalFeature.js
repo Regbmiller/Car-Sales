0c3f963f35057b03d8f93cce628e3f6fa286357a
Your Name <Reginaldbmiller@gmail.com>
2020-04-28 19:57:22 -0400

    redux update

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -4,8 +4,8 @@ import AdditionalFeature from './AdditionalFeature';
 const AdditionalFeatures = props => {
   return (
     <div className="content">
-      <h4>Additional Features</h4>
-      {props.additionalFeatures.length ? (
+       <h4>Additional Features</h4>
+        {props.additionalFeatures.length ? (
         <ol type="1">
           {props.additionalFeatures.map(item => (
             <AdditionalFeature key={item.id} feature={item} addFeature={props.addFeature} />
